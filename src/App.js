2c12e2b64ace6aad7b5626c5e9b4655aa0ea49e6
Michelle Lim <michelle.lim@yale.edu>
2020-01-06 15:08:29 -0800

    Added function that creates new project

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.registerDataset = this.registerDataset.bind(this);
+    this.createProject = this.createProject.bind(this);
   }
 
   registerDataset = () => {
@@ -42,6 +43,28 @@ class App extends React.Component {
     
     }).catch(function(error) {
       console.error("Error adding new dataset: ", error);
+    });
+  };
+
+  createProject = () => {
+    /* Add dataset to datasets */
+    let user_uuid = this.props.user.uid;
+    db.collection('projects').add(
+      {
+        name: "Hardcoded name for now",
+        description: "Hardcoded description",
+        researcher_id: user_uuid,
+        list_of_requests: []
+      }
+    ).then(function(projectRef) {
+      /* Add dataset reference to user */
+      const userRef = db.collection('users').doc(user_uuid)
+      userRef.update({
+        list_of_projects: firebase.firestore.FieldValue.arrayUnion(projectRef)
+      })
+    
+    }).catch(function(error) {
+      console.error("Error creating new project: ", error);
     });
   };
 
@@ -77,11 +100,16 @@ class App extends React.Component {
               ? <button onClick={signOut}>Sign out</button>
               : <button onClick={signInWithGoogle}>Sign in with Google</button>
           }
-
           {/* Owner: Register dataset */}
           {
             user
               ? <button onClick={this.registerDataset}>Register data</button>
+              : ""
+          }
+          {/* Researcher: Start a new project */}
+          {
+            user
+              ? <button onClick={this.createProject}>Create project</button>
               : ""
           }
         </header>
