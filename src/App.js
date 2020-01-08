b7a50e73703ac561094b727b1957d3a280549aac
Michelle Lim <michelle.lim@yale.edu>
2020-01-08 11:12:28 -0800

    Added authentication flow and restricted the profile page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Profile from "./Profile.js";
+import Home from "./Home.js";
+import ProtectedRoute from "./ProtectedRoute.js";
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 
-const Home = () => <div>You're on the Home Tab</div>;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
 class App extends Component {
   render() {
-
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+    const { state = {} } = this.props.location;
+    const { error } = state;
+    /* Register user if not already registered */
+    if (user) {
+      db.collection('users').doc(user.uid).set(
+        {
+          name: user.displayName,
+          photoURL: user.photoURL
+        },
+        {
+          merge: true
+        }
+      )
+    }
     return (
       <div>
         <h1>Silo ML</h1>
@@ -15,14 +44,20 @@ class App extends Component {
           <Link to={`/`} className="link">
             Home
           </Link>
-          <Link to={`/profile`} className="link">
-            Profile
-          </Link>
+          {
+            user 
+              ? (<div>
+                  <Link to={`/profile`} className="link">Profile</Link>
+                  <button onClick={signOut}>Sign out</button>
+                </div>)
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
         </div>
         <div className="tabs">
+        {error && <div>ERROR: {error}</div>}
           <Switch>
             <Route path={`/`} exact component={Home} />
-            <Route path={`/profile`} component={Profile} />
+            <ProtectedRoute path={`/profile`} user={user} component={Profile}/>
           </Switch>
         </div>
       </div>
@@ -30,4 +65,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
