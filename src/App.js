822c7f2c354c92b8b12648a1bc6dc5f735247ba1
dhruvik7 <dhruvik.parikh@hotmail.com>
2020-01-08 11:36:12 -0800

    added project entry

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
+import ProjectEntry from "./profile/ProjectEntry";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
-  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider()
 };
 
 const connection_statuses = {
-  planned: 'planned', // unconnected
-  available: 'available', // owner registered device w OTP, tornado knows connected
-  unavailable: 'unavailable', // connection is in use
-}
+  planned: "planned", // unconnected
+  available: "available", // owner registered device w OTP, tornado knows connected
+  unavailable: "unavailable" // connection is in use
+};
 
 class App extends React.Component {
-
   constructor() {
-    super()
+    super();
     this.createDataset = this.createDataset.bind(this);
     this.createProject = this.createProject.bind(this);
     this.requestForData = this.requestForData.bind(this);
@@ -30,50 +30,49 @@ class App extends React.Component {
     this.rejectRequest = this.rejectRequest.bind(this);
     this.verifyDatasetId = this.verifyDatasetId.bind(this);
   }
-  
+
   createDataset = () => {
     /* Add dataset to datasets */
     let user_uuid = this.props.user.uid;
-    db.collection('datasets').add(
-      {
+    db.collection("datasets")
+      .add({
         name: "Hardcoded name for now",
         description: "Hardcoded description",
         owner_id: user_uuid,
         connection_status: connection_statuses.planned,
         OTP: ""
-
-      }
-    ).then(function(datasetRef) {
-      /* Add dataset reference to user */
-      const userRef = db.collection('users').doc(user_uuid)
-      userRef.update({
-        list_of_datasets: firebase.firestore.FieldValue.arrayUnion(datasetRef)
       })
-    
-    }).catch(function(error) {
-      console.error("Error adding new dataset: ", error);
-    });
+      .then(function(datasetRef) {
+        /* Add dataset reference to user */
+        const userRef = db.collection("users").doc(user_uuid);
+        userRef.update({
+          list_of_datasets: firebase.firestore.FieldValue.arrayUnion(datasetRef)
+        });
+      })
+      .catch(function(error) {
+        console.error("Error adding new dataset: ", error);
+      });
   };
 
   createProject = () => {
     /* Add dataset to datasets */
     let user_uuid = this.props.user.uid;
-    db.collection('projects').add(
-      {
+    db.collection("projects")
+      .add({
         name: "Hardcoded name for now",
         description: "Hardcoded description",
-        researcher_id: user_uuid,
-      }
-    ).then(function(projectRef) {
-      /* Add dataset reference to user */
-      const userRef = db.collection('users').doc(user_uuid)
-      userRef.update({
-        list_of_projects: firebase.firestore.FieldValue.arrayUnion(projectRef)
+        researcher_id: user_uuid
       })
-    
-    }).catch(function(error) {
-      console.error("Error creating new project: ", error);
-    });
+      .then(function(projectRef) {
+        /* Add dataset reference to user */
+        const userRef = db.collection("users").doc(user_uuid);
+        userRef.update({
+          list_of_projects: firebase.firestore.FieldValue.arrayUnion(projectRef)
+        });
+      })
+      .catch(function(error) {
+        console.error("Error creating new project: ", error);
+      });
   };
 
   requestForData = () => {
@@ -83,144 +82,144 @@ class App extends React.Component {
     let researcher_uuid = "Oy3UauTsREglXN6LTqLBI08cdYN2";
     let project_id = "tyWTzzwTdoLYI1RahBtK";
 
-    db.collection('requests').add(
-      {
+    db.collection("requests")
+      .add({
         owner_id: owner_uuid,
         researcher_id: researcher_uuid,
         project_id: project_id,
         dataset_id: dataset_id,
         status: "Pending"
-      }
-    ).then(function(requestRef) {
-      /* Add request reference to project id */
-      const projectRef = db.collection('projects').doc(project_id)
-      projectRef.update({
-        list_of_requests: firebase.firestore.FieldValue.arrayUnion(requestRef)
       })
-      /* Add request reference to dataset id */
-      const datasetRef = db.collection('datasets').doc(dataset_id)
-      datasetRef.update({
-        list_of_requests: firebase.firestore.FieldValue.arrayUnion(requestRef)
+      .then(function(requestRef) {
+        /* Add request reference to project id */
+        const projectRef = db.collection("projects").doc(project_id);
+        projectRef.update({
+          list_of_requests: firebase.firestore.FieldValue.arrayUnion(requestRef)
+        });
+        /* Add request reference to dataset id */
+        const datasetRef = db.collection("datasets").doc(dataset_id);
+        datasetRef.update({
+          list_of_requests: firebase.firestore.FieldValue.arrayUnion(requestRef)
+        });
       })
-    }).catch(function(error) {
-      console.error("Error adding a new request", error);
-    });
+      .catch(function(error) {
+        console.error("Error adding a new request", error);
+      });
   };
 
   approveRequest = () => {
     /* Hardcoded Request*/
     let request_id = "rJe5KSwvfhNXtYBkwn9L";
 
-    db.collection('requests').doc(request_id).update(
-      {
+    db.collection("requests")
+      .doc(request_id)
+      .update({
         status: "Approved"
-      }
-    ).catch(function(error) {
-      console.error("Error approving request", error);
-    });
+      })
+      .catch(function(error) {
+        console.error("Error approving request", error);
+      });
   };
 
   rejectRequest = () => {
     /* Hardcoded Request*/
     let request_id = "Ellmv4rCwTYqAMqJzyex";
 
-    db.collection('requests').doc(request_id).update(
-      {
+    db.collection("requests")
+      .doc(request_id)
+      .update({
         status: "Rejected"
-      }
-    ).catch(function(error) {
-      console.error("Error rejecting request", error);
-    });
+      })
+      .catch(function(error) {
+        console.error("Error rejecting request", error);
+      });
   };
 
-  verifyDatasetId= () => {
+  verifyDatasetId = () => {
     /* Hardcoded Request*/
     let dataset_id = "e2qBRk6SCspTn12XEZw3";
     let OTP = "Nsb4io";
 
-    let datasetRef = db.collection('datasets').doc(dataset_id);
-    datasetRef.get().then(function(doc) {
-      if (doc.exists) {
+    let datasetRef = db.collection("datasets").doc(dataset_id);
+    datasetRef
+      .get()
+      .then(function(doc) {
+        if (doc.exists) {
           console.log("Document data:", doc.data());
-      } else {
+        } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
-      }
-      }).catch(function(error) {
-          console.log("Error getting document:", error);
+        }
+      })
+      .catch(function(error) {
+        console.log("Error getting document:", error);
       });
-
   };
 
-  render(){
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
+  render() {
+    const { user, signOut, signInWithGoogle } = this.props;
 
     /* Register user if not already registered */
     if (user) {
-      db.collection('users').doc(user.uid).set(
-        {
-          name: user.displayName,
-        },
-        {
-          merge: true
-        }
-      )
+      db.collection("users")
+        .doc(user.uid)
+        .set(
+          {
+            name: user.displayName
+          },
+          {
+            merge: true
+          }
+        );
     }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {
-            user 
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+          {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+          {user ? (
+            <button onClick={signOut}>Sign out</button>
+          ) : (
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+          )}
           {/* Owner: Create dataset */}
-          {
-            user
-              ? <button onClick={this.createDataset}>Create new data</button>
-              : ""
-          }
+          {user ? (
+            <button onClick={this.createDataset}>Create new data</button>
+          ) : (
+            ""
+          )}
           {/* Owner: Use OTP to verify the owner dataset id */}
-                    {
-            user
-              ? <button onClick={this.verifyDatasetId}>Verify dataset id</button>
-              : ""
-          }
+          {user ? (
+            <button onClick={this.verifyDatasetId}>Verify dataset id</button>
+          ) : (
+            ""
+          )}
           {/* Researcher: Start a new project */}
-          {
-            user
-              ? <button onClick={this.createProject}>Create project</button>
-              : ""
-          }
+          {user ? (
+            <button onClick={this.createProject}>Create project</button>
+          ) : (
+            ""
+          )}
           {/* Researcher: Send a request to one of the data owners */}
-          {
-            user
-            ? <button onClick={this.requestForData}>Request for Data</button>
-            : ""
-          }
+          {user ? (
+            <button onClick={this.requestForData}>Request for Data</button>
+          ) : (
+            ""
+          )}
           {/* Owner: Approve the request */}
-          {
-            user
-            ? <button onClick={this.approveRequest}>Approve said request</button>
-            : ""
-          }
+          {user ? (
+            <button onClick={this.approveRequest}>Approve said request</button>
+          ) : (
+            ""
+          )}
           {/* Owner: Reject the request */}
-          {
-            user
-            ? <button onClick={this.rejectRequest}>Reject said request</button>
-            : ""
-          }
+          {user ? (
+            <button onClick={this.rejectRequest}>Reject said request</button>
+          ) : (
+            ""
+          )}
         </header>
+        <ProjectEntry db={db}></ProjectEntry>
       </div>
     );
   }
@@ -228,5 +227,5 @@ class App extends React.Component {
 
 export default withFirebaseAuth({
   providers,
-  firebaseAppAuth,
+  firebaseAppAuth
 })(App);
---
+++ src/profile/ProjectEntry.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as firebase from "firebase";
+
+class ProjectEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.project_id = "tyWTzzwTdoLYI1RahBtK";
+    this.colRef = props.db.collection("requests");
+    this.state = {
+      datasets: []
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe1 = this.colRef.onSnapshot(snapshot => {
+      var reqs = [];
+      snapshot.docs.forEach(docSnapshot => {
+        if (docSnapshot.data().project_id === this.project_id) {
+          reqs.push([docSnapshot.data().dataset_id, docSnapshot.data().status]);
+        }
+      });
+      this.setState({
+        datasets: reqs
+      });
+    });
+  }
+  render() {
+    return this.state.datasets.map(pair => (
+      <div>
+        {pair[0]} was {pair[1]}
+      </div>
+    ));
+  }
+}
+
+export default ProjectEntry;
