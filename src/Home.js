432ca6662124c65b22b8ab1fbbf3198e6e5deeef
Michelle Lim <michelle.lim@yale.edu>
2020-01-08 20:36:47 -0800

    Resolved merge conflict in Home.js:

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,44 @@
-import React from "react";
-const Home = props => <div>You're on the Home Tab</div>;
-export default Home;
+
+import React, { Component } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <List dense className={classes.root}>
+      {[0, 1, 2, 3].map(value => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <ListItem key={value} button>
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar n°${value + 1}`}
+                src={`/static/images/avatar/${value + 1}.jpg`}
+              />
+            </ListItemAvatar>
+            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+            <ListItemSecondaryAction>
+              <button>Hello</button>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
